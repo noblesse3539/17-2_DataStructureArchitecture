2939a98f5ae16bc1d643b0ada2f61cf84e8869d4
OpalCat <noblesse3539@daum.net>
2017-09-20 10:37:20 +0900

    appController.java 수정: initCycleDetection 함수 제거, 인스턴스 _pairwiseDisjointSets 삭제

--- src/_Main_DS2P02_201100964.java
+++ src/_Main_DS2P02_201100964.java
@@ -1,5 +1,5 @@
 
-public class _Main_DS2P01_201100964 {
+public class _Main_DS2P02_201100964 {
 	public static void main(String[] args) {
 		AppController appController = new AppController();
 		appController.run();
